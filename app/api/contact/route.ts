bf921a7dc04d1e08897e9f6490ba76e1a012b014
Alba <100930901+albadelatorres@users.noreply.github.com>
2025-03-19 12:21:05 +0100

    sendgrid documentation save me

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,35 +1,33 @@
-// pages/api/contact.ts
-import sgMail from "@sendgrid/mail";
+import sgMail, { MailDataRequired } from "@sendgrid/mail";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 export async function POST(request: Request) {
   try {
-    // Parse the JSON body from the request
     const { navn, email, telefon, postnummer, besked } = await request.json();
 
-    // Create a single message with 2 personalizations
-    const msg = {
-      to: [email, process.env.SENDGRID_FROM_EMAIL as string],
-      // The verified sender
+    const msg: MailDataRequired = {
+      personalizations: [
+        {
+          to: [{ email }],
+          cc: [{ email: process.env.SENDGRID_FROM_EMAIL as string }],
+          subject: "Vi har modtaget din besked",
+        },
+      ],
       from: process.env.SENDGRID_FROM_EMAIL as string,
-
-      // We’ll reuse the same subject/content for both.
-      subject: "Vi har modtaget din besked",
       text: `Hej ${navn},
 
-        Vi har modtaget din besked og vil kontakte dig snarest. Tak for din interesse i Fugemester Clausen!
+Vi har modtaget din besked og vil kontakte dig snarest. Tak for din interesse i Fugemester Clausen!
 
-        Her er en kopi af de oplysninger, du har indsendt:
-        Navn: ${navn}
-        Email: ${email}
-        Telefon: ${telefon}
-        Postnummer: ${postnummer}
-        Besked: ${besked}
+Her er en kopi af de oplysninger, du har indsendt:
+Navn: ${navn}
+Email: ${email}
+Telefon: ${telefon}
+Postnummer: ${postnummer}
+Besked: ${besked}
 
-        Med venlig hilsen,
-        Daniel`,
-
+Med venlig hilsen,
+Daniel`,
       html: `<p>Hej ${navn},</p>
              <p>Vi har modtaget din besked og vil kontakte dig snarest. Tak for din interesse i <strong>Fugemester Clausen</strong>!</p>
              <p>Her er en kopi af de oplysninger, du har indsendt:</p>
@@ -43,8 +41,7 @@ export async function POST(request: Request) {
              <p>Med venlig hilsen,<br/>Daniel</p>`,
     };
 
-    // Send both emails in one go
-    await sgMail.sendMultiple(msg);
+    await sgMail.send(msg);
 
     return new Response(JSON.stringify({ message: "Emails sent successfully" }), {
       status: 200,
